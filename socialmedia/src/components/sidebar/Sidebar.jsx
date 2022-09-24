8f3e7e1af512ec8e598f42e5ed4db05219785cae
narangoo <is21d005@mandakh.edu.mn>
2022-09-24 18:13:40 +0800

    up

--- socialmedia/src/components/sidebar/Sidebar.jsx
+++ socialmedia/src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import "./sidebar.css"
-import {RssFeed} from "@mui/icons-material"
+import {RssFeed,PlayCircleOutline,Group,Bookmark,HelpOutline,WorkOutline,Event,School} from "@mui/icons-material"
 
 export default function Sidebar() {
   return (
@@ -7,9 +7,45 @@ export default function Sidebar() {
       <div className="sidebarWrapper">
         <ul className="sidebarList">
           <li className="sidebarListItem">
-            <RssFeed/>
-            <span className="sidebarListItemText"></span>
+            <RssFeed className="sidebarIcon" />
+            <span className="sidebarListItemText">Feed</span>
           </li>
+          <li className="sidebarListItem">
+            <PlayCircleOutline className="sidebarIcon" />
+            <span className="sidebarListItemText">Videos</span>
+          </li>
+          <li className="sidebarListItem">
+            <Group className="sidebarIcon" />
+            <span className="sidebarListItemText">Group</span>
+          </li>
+          <li className="sidebarListItem">
+            <Bookmark className="sidebarIcon" />
+            <span className="sidebarListItemText">Bookmarks</span>
+          </li>
+          <li className="sidebarListItem">
+            <HelpOutline className="sidebarIcon" />
+            <span className="sidebarListItemText">Questions</span>
+          </li>
+          <li className="sidebarListItem">
+            <WorkOutline className="sidebarIcon" />
+            <span className="sidebarListItemText">Jobs</span>
+          </li>
+          <li className="sidebarListItem">
+            <Event className="sidebarIcon" />
+            <span className="sidebarListItemText">Events</span>
+          </li>
+          <li className="sidebarListItem">
+            <School className="sidebarIcon" />
+            <span className="sidebarListItemText">Courses</span>
+          </li>
+          <button className="sidebarButton">Show More</button>
+          <hr className="sidebarHr"/>
+          <ul className="sidebarFriendList">
+            <li className="sidebarFriend">
+              <img className="sidebarFriendImg" src="assets/" alt="" />
+              <span className="sidebarFriendName">Jane Doe</span>
+            </li>
+          </ul>
         </ul>
       </div>
     </div>
