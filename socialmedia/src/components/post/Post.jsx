4d58bf11e7afb8c4aef2072e1c8af22ab1621806
narangoo <is21d005@mandakh.edu.mn>
2022-09-25 15:27:11 +0800

    up

--- socialmedia/src/components/post/Post.jsx
+++ socialmedia/src/components/post/Post.jsx
@@ -1,32 +1,40 @@
 import "./post.css"
 import {MoreVert} from "@mui/icons-material"
+import {Users} from "../../dummyData"
 
-export default function Post() {
+export default function Post({post}) {
+
   return (
     <div className="post">
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            <img className="postProfileImg" src="/assets/person/1.png" alt="" />
-            <span className="postUsername">Hannah Baker</span>
-            <span className="postDate">5 mins ago</span>
+            <img 
+              className="postProfileImg" 
+              src={Users.filter(u=>u.id === post.userId)[0].profilePicture} 
+              alt="" 
+            />
+            <span className="postUsername">
+              {Users.filter(u=>u.id === post.userId)[0].username}
+            </span>
+            <span className="postDate">{post.date}</span>
           </div>
           <div className="postTopRight">
             <MoreVert/>
           </div>
         </div>
         <div className="postCenter">
-          <span className="postText">Hey! Its my first post:)</span>
-          <img className="postImg" src="/assets/post/2.jpeg" alt="" />
+          <span className="postText">{post?.desc}</span>
+          <img className="postImg" src={post.photo} alt="" />
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
             <img className="likeIcon" src="assets/likee.jpg" alt="" />
             <img className="likeIcon" src="assets/heart.jpg" alt="" />
-            <span className="postLikeCounter">32 people like it</span>
+            <span className="postLikeCounter">{post.like} people like it</span>
           </div>
           <div className="postBottomRight">
-            <span className="postCommentText">9 comments</span>
+            <span className="postCommentText">{post.comment} comments</span>
           </div>
         </div>
       </div>
---
+++ socialmedia/src/dummyData.js
@@ -0,0 +1,99 @@
+export const Users=[
+    {
+        id: 1,
+        profilePicture: "assets/person/1.png",
+        username: "Hannah Baker",
+    },
+    {
+        id: 2,
+        profilePicture: "assets/person/2.jpg",
+        username: "Dwayne Johnson",
+    },
+    {
+        id: 3,
+        profilePicture: "assets/person/3.jpg",
+        username: "Jackie Chan",
+    },
+    {
+        id: 4,
+        profilePicture: "assets/person/4.png",
+        username: "Alex Durden",
+    },
+    {
+        id: 5,
+        profilePicture: "assets/person/5.jpg",
+        username: "Jane Doe",
+    },
+    {
+        id: 6,
+        profilePicture: "assets/person/6.jpeg",
+        username: "Max Thomas",
+    },
+    {
+        id: 7,
+        profilePicture: "assets/person/7.jpg",
+        username: "Mary Jerry",
+    },
+    {
+        id: 8,
+        profilePicture: "assets/person/8.jpg",
+        username: "Will Smith",
+    },
+    {
+        id: 9,
+        profilePicture: "assets/person/9.jpg",
+        username: "Someone",
+    },
+    {
+        id: 10,
+        profilePicture: "assets/person/oppa.jpg",
+        username: "Lee Jongsuk",
+    },
+];
+
+export const Posts=[
+    {
+        id: 1,
+        desc: "Nice place and Weather:)",
+        photo:"assets/post/2.jpeg",
+        date:"2 mins ago",
+        userId: 10,
+        like:1000,
+        comment: 600,
+    },
+    {
+        id: 2,
+        desc:"<3<3",
+        photo:"assets/post/1.jpeg",
+        date:"15 mins ago",
+        userId: 4,
+        like: 6,
+        comment: 1,
+    },
+    {
+        id: 3,
+        desc: ":)",
+        photo:"assets/post/a.jpg",
+        date:"1 hour ago",
+        userId: 2,
+        like: 3000,
+        comment: 800,
+    },
+    {
+        id: 4,
+        desc: "Lovelyy:)",
+        photo:"assets/post/4.jpeg",
+        date:"4 hours ago",
+        userId: 7,
+        like: 32,
+        comment: 6,
+    },
+    {
+        id: 5,
+        photo:"assets/post/5.jpeg",
+        date:"5hours ago",
+        userId: 3,
+        like: 500,
+        comment: 323,
+    },
+]
