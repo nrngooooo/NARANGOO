a2dceffd965da7843c397844fe269e34d206b5a1
narangoo <is21d005@mandakh.edu.mn>
2022-09-25 19:56:43 +0800

    up

---
+++ socialmedia/src/pages/login/Login.jsx
@@ -0,0 +1,18 @@
+import "./login.css"
+
+export default function Login() {
+  return (
+    <div className="login">
+        <div className="loginWrapper">
+            <div className="loginLeft">
+                <h3 className="loginLogo">Narangoosocial</h3>
+                <span className="loginDesc">
+                Connect with friends and the world around you on Narangoosocial.
+                </span>
+            </div>
+            <div className="loginRight"></div>
+                <div className="loginBox"></div>
+        </div>
+    </div>
+  )
+}
