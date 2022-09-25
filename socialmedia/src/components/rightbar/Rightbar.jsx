c4c9d3232949af309e0a120924b796cab75d57de
narangoo <is21d005@mandakh.edu.mn>
2022-09-25 19:28:24 +0800

    up

--- socialmedia/src/components/rightbar/Rightbar.jsx
+++ socialmedia/src/components/rightbar/Rightbar.jsx
@@ -2,11 +2,13 @@ import "./rightbar.css"
 import {Users} from "../../dummyData"
 import Online from "../online/Online"
 
-export default function Rightbar() {
-  return (
-    <div className="rightbar">
-      <div className="rightbarWrapper">
-        <div className="birthdayContainer">
+
+export default function Rightbar({profile}) {
+
+  const HomeRightbar = () =>{
+    return(
+      <>
+      <div className="birthdayContainer">
           <img className="birthdayImg" src="assets/gift.png" alt="" />
           <span className="birthdayText">
             <b>Pola Foster</b> and <b>3 other friends</b> have a birthday today.
@@ -19,6 +21,62 @@ export default function Rightbar() {
             <Online key={u.id} user={u} />
           ))}
         </ul>
+      </>
+    );
+  };
+
+  const ProfileRightbar = () =>{
+    return(
+    <>
+    <h4 className="rightbarTitle">User information</h4>
+    <div className="rightbarInfo">
+      <div className="rightbarInfoItem">
+        <span className="rightbarInfoKey">City:</span>
+        <span className="rightbarInfoValue">New York</span>
+      </div>
+      <div className="rightbarInfoItem">
+        <span className="rightbarInfoKey">From:</span>
+        <span className="rightbarInfoValue">Madrid</span>
+      </div>
+      <div className="rightbarInfoItem">
+        <span className="rightbarInfoKey">Relationship:</span>
+        <span className="rightbarInfoValue">Single</span>
+      </div>
+    </div>
+    <h4 className="rightbarTitle">User friends</h4>
+    <div className="rightbarFollowings">
+      <div className="rightbarFollowing">
+        <img src="assets/person/oppa.jpg" alt="" className="rightbarFollowingImg" />
+        <span className="rightbarFollowingName">Lee Jongsuk</span>
+      </div>
+      <div className="rightbarFollowing">
+        <img src="assets/person/2.jpg" alt="" className="rightbarFollowingImg" />
+        <span className="rightbarFollowingName">Lee Jongsuk</span>
+      </div>
+      <div className="rightbarFollowing">
+        <img src="assets/person/3.jpg" alt="" className="rightbarFollowingImg" />
+        <span className="rightbarFollowingName">Lee Jongsuk</span>
+      </div>
+      <div className="rightbarFollowing">
+        <img src="assets/person/4.png" alt="" className="rightbarFollowingImg" />
+        <span className="rightbarFollowingName">Lee Jongsuk</span>
+      </div>
+      <div className="rightbarFollowing">
+        <img src="assets/person/5.jpg" alt="" className="rightbarFollowingImg" />
+        <span className="rightbarFollowingName">Lee Jongsuk</span>
+      </div>
+      <div className="rightbarFollowing">
+        <img src="assets/person/7.jpg" alt="" className="rightbarFollowingImg" />
+        <span className="rightbarFollowingName">Lee Jongsuk</span>
+      </div>
+    </div>
+    </>
+  )
+  }
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <ProfileRightbar/>
       </div>
     </div>
   )
--- socialmedia/src/pages/profile/Profile.jsx
+++ socialmedia/src/pages/profile/Profile.jsx
@@ -11,10 +11,19 @@ export default function Profile() {
     <div className="profile">
       <Sidebar/>
       <div className="profileRight">
-        <div className="profileRightTop">hhgh</div>
+        <div className="profileRightTop">
+            <div className="profileCover">
+            <img className="profileCoverImg" src="assets/post/a.jpg" alt="" />
+            <img className="profileUserImg" src="assets/person/1.png" alt="" />
+            </div>
+            <div className="profileInfo">
+                <h4 className="profileInfoName">Hannah Baker</h4>
+                <span className="profileInfoDesc">Hello my friends!</span>
+            </div>
+        </div>
         <div className="profileRightBottom">
             <Feed/>  
-            <Rightbar/>
+            <Rightbar profile/>
         </div>
       </div>
     </div>
