a86ba5d0f4b5f8ddf55c5184483beabd72fd0a7d
nrngooooo <is21d005@mandakh.edu.mn>
2022-09-29 18:20:19 +0800

    up

--- socialmedia/src/App.js
+++ socialmedia/src/App.js
@@ -1,6 +1,9 @@
 import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
  function App() {
-  return <Home/>
+  return <Register/>
 }
 
 export default App;
