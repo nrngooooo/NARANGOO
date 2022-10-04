641e05df3edf52ad9fb00fee008a7728b7738239
Narangoo <is21d005@mandakh.edu.mn>
2022-10-04 19:01:25 +0800

    jh

---
+++ weather/src/components/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+        'X-RapidAPI-Key': '25191535b1msh5bed711ecbbbdfep170593jsn0fef28b92f76',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL ="https://wft-geo-db.p.rapidapi.com/v1/geo";
+
--- weather/src/components/search/Search.js
+++ weather/src/components/search/Search.js
@@ -1,9 +1,18 @@
 import { useState } from "react";
 import {AsyncPaginate} from "react-select-async-paginate"
+import {GEO_API_URL, geoApiOptions} from "../../api";
 
 const Search = ({onSearchChange}) =>{
 
     const [search, setSearch] = useState(null);
+
+    const loadOptions = {inputValue} =>{
+        return fetch('${GEO_API_URL}/cities?', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err))
+       
+    }
 
     const handleOnChange = (searchData) => {
         setSearch(searchData);
@@ -15,8 +24,9 @@ const Search = ({onSearchChange}) =>{
         debounceTimeout={600}
         value={search}
         onChange={handleOnChange}
+        loadOptions={loadOptions}
         />
-    )
-}
+    )  
+};
 
 export default Search;
