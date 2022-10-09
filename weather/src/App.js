1739c1b26dbfd929a75027b6f4f3c70865b6e1a4
nrngooooo <is21d005@mandakh.edu.mn>
2022-10-09 16:40:23 +0800

    a

--- weather/src/App.js
+++ weather/src/App.js
@@ -2,21 +2,34 @@ import './App.css';
 import Search from './components/search/Search';
 import CurrentWeather from './components/current-weather/current-weather';
 import { WEATHER_API_URL, WEATHER_API_KEY } from './api';
+import { useState } from 'react';
+import Forecast from './components/forecast/forecast';
 
 function App() {
+  const [currentWeather,setcurrentWeather] = useState(null);
+  const [forecast,setForecast] = useState(null);
 
   const handleOnSearchChange = (searchData) =>{
     const [lat, lon] = searchData.value.split("");
 
-    const CurrentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
-    const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
-    Promise.all([CurrentWeatherFetch, forecastFetch])
+    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
+    const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async(response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+        setcurrentWeather({ city: searchData.label, ...weatherResponse});
+        setForecast({ city: searchData.label, ...forecastResponse});
+      })
+      .catch((err) => console.log(err));
   }
+  console.log(forecast);
 
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange}/>
-      <CurrentWeather/> 
+      {currentWeather && <CurrentWeather data={currentWeather}/>}
+      <Forecast/>
     </div>
   );
 }
---
+++ weather/src/components/forecast/forecast.js
@@ -0,0 +1,4 @@
+const Forecast = () => {
+    return 'hello'
+}
+export default Forecast;
