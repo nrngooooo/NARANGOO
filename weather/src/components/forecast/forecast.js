ec2e8626488cebc4467fa976cf2eab15f607ad73
nrngooooo <is21d005@mandakh.edu.mn>
2022-10-09 18:42:39 +0800

    a

--- weather/src/components/forecast/forecast.js
+++ weather/src/components/forecast/forecast.js
@@ -1,5 +1,29 @@
-import { Accordion, AccordionItemHeading, AccordionItem, AccordionItemPanel, AccordionItemButton } from "react-accessible-accordion"
+import {
+    Accordion,
+    AccordionItemHeading,
+    AccordionItem,
+    AccordionItemPanel,
+    AccordionItemButton
+}
+from "react-accessible-accordion"
+import "./forecast.css";
+
+const WEEK_DAYS = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday'
+];
+
 const Forecast = ({ data }) => {
+    const dayInAWeek = new Date().getDay();
+    const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(
+        WEEK_DAYS.slice(0, dayInAWeek)
+    );
+
     return (
         <>
             <label className="title">Daily</label>
@@ -9,7 +33,14 @@ const Forecast = ({ data }) => {
                         <AccordionItemHeading>
                             <AccordionItemButton>
                                 <div className="daily-item">
-                                    <img alt="weather" className="icon-small" src={`icons/${item.weather[0].icon}.png`}/>
+                                    <img alt="weather" className="icon-small" src={`icons/${item.weather[0].icon}.png`} />
+                                    <label className="day">{forecastDays[idx]}</label>
+                                    <label className="description">
+                                        : {item.weather[0].description}
+                                    </label>
+                                    <label className="min-max">
+                                        {Math.round(item.main.temp_min)}°C /{""}
+                                        {Math.round(item.main.temp_max)}°C</label>
                                 </div>
                             </AccordionItemButton>
                         </AccordionItemHeading>
