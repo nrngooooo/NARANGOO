3dfa6ff3e801735a69a750eb0049cffa0ab5173f
nrngooooo <is21d005@mandakh.edu.mn>
2022-10-09 17:38:22 +0800

    qwerty

--- weather/src/components/forecast/forecast.js
+++ weather/src/components/forecast/forecast.js
@@ -1,4 +1,23 @@
-const Forecast = () => {
-    return 'hello'
+import { Accordion, AccordionItemHeading, AccordionItem, AccordionItemPanel, AccordionItemButton } from "react-accessible-accordion"
+const Forecast = ({ data }) => {
+    return (
+        <>
+            <label className="title">Daily</label>
+            <Accordion allowZeroExpanded>
+                {data.list.splice(0, 7).map((item, idx) => (
+                    <AccordionItem key={idx}>
+                        <AccordionItemHeading>
+                            <AccordionItemButton>
+                                <div className="daily-item">
+                                    <img alt="weather" className="icon-small" src={`icons/${item.weather[0].icon}.png`}/>
+                                </div>
+                            </AccordionItemButton>
+                        </AccordionItemHeading>
+                        <AccordionItemPanel></AccordionItemPanel>
+                    </AccordionItem>
+                ))}
+            </Accordion>
+        </>
+    )
 }
 export default Forecast;
