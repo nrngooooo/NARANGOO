6beec748016ab27f90742bc403914c84da308494
nrngooooo <is21d005@mandakh.edu.mn>
2022-10-09 15:10:04 +0800

    a

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import Search from './components/search/Search';
 import CurrentWeather from './components/current-weather/current-weather';
+import { WEATHER_API_URL, WEATHER_API_KEY } from './api';
 
 function App() {
 
   const handleOnSearchChange = (searchData) =>{
-    console.log(searchData)
+    const [lat, lon] = searchData.value.split("");
+
+    const CurrentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
+    const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
+    Promise.all([CurrentWeatherFetch, forecastFetch])
   }
 
   return (
