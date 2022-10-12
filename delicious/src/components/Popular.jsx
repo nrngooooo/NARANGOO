d164291da99e3cca65d1cbdc1aca4bba403f8fd3
nrngooooo <is21d005@mandakh.edu.mn>
2022-10-12 22:53:59 +0800

    g

--- delicious/src/components/Popular.jsx
+++ delicious/src/components/Popular.jsx
@@ -88,7 +88,7 @@ const Card = styled.div`
 const Gradient = styled.div`
   z-index: 3;
   position: absolute;
-  width: 100%;
+  width: 100%
   height: 100%;
   background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
 `;
