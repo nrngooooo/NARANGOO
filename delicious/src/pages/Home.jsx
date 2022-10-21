820886de1106202cdd0c42dd4aa6951c4665ac77
nrngooooo <is21d005@mandakh.edu.mn>
2022-10-21 22:44:36 +0800

    n

--- delicious/src/pages/Home.jsx
+++ delicious/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import Veggie from "../components/Veggie";
 import Popular from "../components/Popular";
-
 import React from "react";
+import Category from "../components/Category";
 
 function Home() {
   return (
     <div>
+      <Category/>
       <Veggie />
       <Popular />
     </div>
