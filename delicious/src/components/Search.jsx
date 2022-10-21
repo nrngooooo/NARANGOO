aca098bf4420d79273a027bf581c84033288f463
nrngooooo <is21d005@mandakh.edu.mn>
2022-10-22 00:02:05 +0800

    dd

--- delicious/src/components/Search.jsx
+++ delicious/src/components/Search.jsx
@@ -3,16 +3,40 @@ import { useStates } from "react";
 import { FaSearch } from "react-icons/fa";
 
 function Search() {
-  return (
-    <form>
-        <input type="text" />
-    </form>
-  )
+    return (
+        <FormStyle>
+            <div>
+                <FaSearch></FaSearch>
+                <input type="text" />
+            </div>
+        </FormStyle>
+    )
 }
 
 const FormStyle = styled.form`
     margin: 0rem 20rem;
-    position: relative;
-`
+    div{
+        width: 100%;
+        position: relative;
+    }
+    input{
+        border: none;
+        background: linear-gradient(35deg, #494949, #313131);
+        font-size: 1.5rem;
+        color: white;
+        padding: 1rem 3rem;
+        border: none;
+        border-radius: 1rem;
+        outline: none;
+        width: 100%;
+    }
+    svg{
+        position: absolute;
+        top: 50%;
+        left: 0%;
+        transform: translate(100%, -50%);
+        color: white;
+    }
+`;
 
 export default Search
