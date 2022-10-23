a83debf0762e9497298bbdb555e6bce81e6078d4
nrngooooo <is21d005@mandakh.edu.mn>
2022-10-23 15:57:13 +0800

    bvg

--- ecommerce/components/Navbar.jsx
+++ ecommerce/components/Navbar.jsx
@@ -5,11 +5,14 @@ import { AiOutlineShopping } from "react-icons/ai"
 const Navbar = () => {
   return (
     <div className='navbar-container'>
-      <p>
-        <Link>
-          
-        </Link>
+      <p className='logo'>
+        <Link href="/">JSM Headphones</Link>
       </p>
+
+      <button type='button' className='cart-icon' onClick="">
+        <AiOutlineShopping/>
+        <span className='cart-item-qty'>1</span>
+      </button>
     </div>
   )
 }
