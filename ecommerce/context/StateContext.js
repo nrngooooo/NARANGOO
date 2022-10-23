53c846c1efa95b49a8ac5e457eb75887363f46a1
nrngooooo <is21d005@mandakh.edu.mn>
2022-10-23 19:49:38 +0800

    hbhb

--- ecommerce/context/StateContext.js
+++ ecommerce/context/StateContext.js
@@ -5,9 +5,9 @@ const Context = createContext();
 
 export const StateContext = ({children}) =>{
     const [showCart, setShowCart] = useState(false);
-    const [cartItems, setCartItems] = useState();
+    const [cartItems, setCartItems] = useState([]);
     const [totalPrice, setTotalPrice] = useState();
-    const [totalQuantities, setTotalQuantities] = useState();
+    const [totalQuantities, setTotalQuantities] = useState(0);
     const [qty, setQty] = useState(1);
 
     const onAdd = (product, quantity) =>{
@@ -50,6 +50,7 @@ export const StateContext = ({children}) =>{
     return(
         <Context.Provider value={{
             showCart,
+            setShowCart,
             cartItems,
             totalPrice,
             totalQuantities,
