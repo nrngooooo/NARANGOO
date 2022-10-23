ceabb7d41c65d4008f39cffd8d7d5d5cc96ae8b6
nrngooooo <is21d005@mandakh.edu.mn>
2022-10-23 19:04:41 +0800

    n

--- ecommerce/pages/_app.js
+++ ecommerce/pages/_app.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import '../styles/globals.css'
-import { Layout } from '../components'
+import React from 'react';
+import {Toaster} from 'react-hot-toast';
+
+import '../styles/globals.css';
+import { Layout } from '../components';
+import { StateContext } from '../context/StateContext';
 
 function MyApp({ Component, pageProps }) {
-  return(
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>
-)}
+  return (
+    <StateContext>
+      <Layout>
+      <Toaster/>
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  )
+}
 
 export default MyApp
