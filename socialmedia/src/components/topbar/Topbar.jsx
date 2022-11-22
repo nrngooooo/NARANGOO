b7818079b4eaeac8e62f5119318e3edf11e27a4b
nrngooooo <is21d005@mandakh.edu.mn>
2022-11-22 12:58:49 +0800

    asfd ,m

--- socialmedia/src/components/topbar/Topbar.jsx
+++ socialmedia/src/components/topbar/Topbar.jsx
@@ -1,10 +1,10 @@
 import "./topbar.css"
 import {Search,Person,Chat,Notifications} from "@mui/icons-material"
-export default function Topbar({sum}) {
+export default function Topbar() {
   return (
     <div  className="topbarContainer">
         <div className="topbarLeft">
-          <span className="logo">{sum}</span>
+          <span className="logo">Narangoosocial</span>
         </div>
         <div className="topbarCenter">
           <div className="searchbar">
--- socialmedia/src/pages/home/Home.jsx
+++ socialmedia/src/pages/home/Home.jsx
@@ -5,8 +5,18 @@ import Rightbar from "../../components/rightbar/Rightbar";
 import "./Home.css"
 import { useState, useEffect } from "react";
 import { sendRequest, urlLookup } from "../../settings/settings"
-export default function Home() {
+import { Button, notification } from 'antd';
 
+export default function Home() {
+  const openNotification = ((title,desc) => {
+    notification.open({
+      message: title,
+      description: desc,
+      onClick: () => {
+        console.log(desc+'Clicked!');
+      },
+    });
+  });
   const [datas, setDatas] = useState();
 
   useEffect(() => {
@@ -16,22 +26,22 @@ export default function Home() {
 
     sendRequest(urlLookup, bodyChiglel).then((data) => setDatas(data));
   }, []);
-  const DisplayData = datas && datas.data.map((gazar) => {
-    return (
-      <div key={gazar.chiglelnum}>
-        <h5><button onClick={() => console.log(gazar.chiglelnum)}>
-          {gazar.chiglelname}</button>
-        </h5>
-        <br />
-      </div>
-    )
+    const DisplayData = datas && datas.data.map((gazar) => {
+      return (
+        <div key={gazar.chiglelnum}>
+          <h5><Button type="primary" onClick={()=>openNotification(gazar.chiglelnum,gazar.chiglelname)}>{gazar.chiglelnum}. 
+            {gazar.chiglelname}</Button>
+          </h5>
+          <br />
+        </div>
+      )
   });
 
   return (
     <>
       {DisplayData}
       <p>{datas && JSON.stringify(datas)}</p>
-      <Topbar sum={datas && datas.data[9].sumduud[3]['sumname']} />
+      <Topbar/>
       <div className="homeContainer">
         <Sidebar />
         <Feed />
